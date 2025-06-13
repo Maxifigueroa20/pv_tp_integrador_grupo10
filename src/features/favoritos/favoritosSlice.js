2f7871a379b7dfbc588b972236d444aff0d1d412
Maxifigueroa20 <marianof0101@gmail.com>
2025-06-13 19:47:32 -0300

    feat: se desarrolló la lógica para el slice de favoritos [MaximilianoFigueroa]

--- src/features/favoritos/favoritosSlice.js
+++ src/features/favoritos/favoritosSlice.js
@@ -8,10 +8,22 @@ export const favoritosSlice = createSlice({
   name: "favoritos",
   initialState,
   reducers: {
-
+    agregarFavorito: (state, action) => {
+      const existe = state.favoritos.some(
+        (favorito) => favorito.id === action.payload.id
+      )
+      if (!existe) {
+        state.favoritos.push(action.payload)
+      }
+    },
+    eliminarFavorito: (state, action) => {
+      state.favoritos = state.favoritos.filter(
+        (favorito) => favorito.id !== action.payload.id
+      )
+    }
   }
 })
 
-export const {  } = favoritosSlice.actions
+export const { agregarFavorito, eliminarFavorito } = favoritosSlice.actions
 
 export default favoritosSlice.reducer
