1c04761ac132bd21717a961132f72c4f024a92a8
Huells2252 <huellsnicolasr@gmail.com>
2025-06-13 19:25:57 -0300

    feat: agregar configuración básica de rutas con React Router y estructura principal [Huells Nicolas]

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import Inicio from "./pages/Inicio.jsx";
+import Favoritos from "./pages/Favoritos.jsx";
+import Detalle from "./pages/Detalle.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Inicio /> },
+      { path: "favoritos", element: <Favoritos /> },
+      { path: "detalle/:id", element: <Detalle /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
