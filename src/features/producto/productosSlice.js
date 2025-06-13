e24ee7c9ccdda0ad2ce6bbb5485a764271be902e
Maxifigueroa20 <marianof0101@gmail.com>
2025-06-13 19:52:58 -0300

    feat: se desarrolló lógica para manejo local de productos con reducers [MaximilianoFigueroa]

--- src/features/producto/productosSlice.js
+++ src/features/producto/productosSlice.js
@@ -8,10 +8,19 @@ export const productosSlice = createSlice({
     name: 'productos',
     initialState,
     reducers: {
-
+        agregarProducto: (state, action) => {
+            state.productos.push(action.payload)
+        },
+        eliminarProducto: (state, action) => {
+            state.productos = state.productos.filter(producto => producto.id !== action.payload)
+        },
+        actualizarProducto: (state, action) => {
+            const actualizado = action.payload
+            state.productos = state.productos.map(producto => producto.id === actualizado.id ? actualizado : producto)
+        }
     }
 })
 
-export const {  } = productosSlice.actions
+export const { agregarProducto, eliminarProducto, actualizarProducto } = productosSlice.actions
 
 export default productosSlice.reducer
