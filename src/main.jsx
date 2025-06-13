ecdaf6adc373db80fa298e41efc96b1939278a02
Huells2252 <huellsnicolasr@gmail.com>
2025-06-13 19:28:17 -0300

    feat: integrar Redux Provider y cargar estilos y scripts de Bootstrap [Huells Nicolas]

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,11 @@ import App from "./App.jsx";
 import Inicio from "./pages/Inicio.jsx";
 import Favoritos from "./pages/Favoritos.jsx";
 import Detalle from "./pages/Detalle.jsx";
+import { Provider } from "react-redux";
+import { store } from "./app/store.js";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +25,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
