01aa09cac30cdc2c474940ac57e235e302589c08
Maxifigueroa20 <marianof0101@gmail.com>
2025-07-02 20:17:32 -0300

    feat: se desarrolló la lógica para la autenticación de usuarios [MaximilianoFigueroa]

--- src/features/autenticacion/authSlice.js
+++ src/features/autenticacion/authSlice.js
@@ -1,17 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit"
 
+const getSession = () => {
+    const session = localStorage.getItem("sessionUser")
+    return session ? JSON.parse(session) : null
+}
+
 const initialState = {
-    user: null
+    user: getSession(),
 }
 
 export const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
-
+        login: (state, action) => {
+            state.user = action.payload
+            localStorage.setItem("sessionUser", JSON.stringify(action.payload))
+        },
+        logout: (state) => {
+            state.user = null
+            localStorage.removeItem("sessionUser")
+        }
     }
 })
 
-export const { } = authSlice.actions
+export const { login, logout } = authSlice.actions
 
 export default authSlice.reducer
