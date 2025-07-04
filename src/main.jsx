c3daf7a67a8688e76873fe67d775c178537f1ebc
Huells2252 <huellsnicolasr@gmail.com>
2025-07-04 01:42:51 -0300

    chore: estructura inicial con Redux, React Router y estilos Bootstrap [Huells Nicolas Rafael]

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,76 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import { store } from "./app/store.js"
-import { Provider } from "react-redux"
-import App from "./App.jsx"
-import Inicio from "./pages/Inicio.jsx"
-import Favoritos from "./pages/Favoritos.jsx"
-import Detalle from "./pages/Detalle.jsx"
-import FormularioProducto from "./pages/FormularioProducto.jsx"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { store } from "./app/store.js";
+import { Provider } from "react-redux";
+import App from "./App.jsx";
+import Inicio from "./pages/Inicio.jsx";
+import Favoritos from "./pages/Favoritos.jsx";
+import Detalle from "./pages/Detalle.jsx";
+import FormularioProducto from "./pages/FormularioProducto.jsx";
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.bundle.min.js"
-import "bootstrap-icons/font/bootstrap-icons.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children: [
-            { index: true, element: <Inicio /> },
-            { path: "favoritos", element: <Favoritos /> },
-            { path: "detalle/:id", element: <Detalle /> },
-            { path: "crear", element: <FormularioProducto /> },
-            { path: "editar/:id", element: <FormularioProducto /> },
-        ],
-    },
-])
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "login", element: <Login /> }, // pública
+      { path: "register", element: <Register /> }, // pública
+      {
+        index: true,
+        element: (
+          <PrivateRoute>
+            <Inicio />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "favoritos",
+        element: (
+          <PrivateRoute>
+            <Favoritos />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "detalle/:id",
+        element: (
+          <PrivateRoute>
+            <Detalle />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "crear",
+        element: (
+          <PrivateRoute>
+            <FormularioProducto />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "editar/:id",
+        element: (
+          <PrivateRoute>
+            <FormularioProducto />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <Provider store={store}>
-            <RouterProvider router={router} />
-        </Provider>
-    </StrictMode>
-)
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
